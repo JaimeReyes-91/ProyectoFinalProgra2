2debfda962118922444f8426bb779704090baa1d
Jaime Reyes-91 <jreyesr26@miumg.edu.gt>
2025-10-04 20:11:18 -0600

    Primera subida del proyecto, Empleado y Proveedor

---
+++ src/proyectofinal/Main.java
@@ -0,0 +1,21 @@
+
+package proyectofinal;
+
+import Conexion.Conexion;
+import Formulario.Proveedor;
+
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+public class Main {
+
+    public static void main(String[] args)throws SQLException {
+        Conexion conexionPostgres = new Conexion();
+        Connection con = conexionPostgres.getConexion();
+        
+    }
+    
+}
